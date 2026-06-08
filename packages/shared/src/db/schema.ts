cb3c413475f7369066c3c99dee0f770cba494b7d
Arun Kumar <pheron408@gmail.com>
2026-06-08 17:10:51 +0530

    feat(schema): add delivery_attempts table schema and status enum

--- packages/shared/src/db/schema.ts
+++ packages/shared/src/db/schema.ts
@@ -1,11 +1,18 @@
 import {
   integer,
   json,
+  pgEnum,
   pgTable,
   text,
   timestamp,
   uuid,
 } from "drizzle-orm/pg-core";
+
+export const status = pgEnum("status", [
+  "delivered",
+  "failed",
+  "dead_lettered",
+]);
 
 export const projects = pgTable("projects", {
   id: uuid().defaultRandom().primaryKey(),
@@ -22,4 +29,21 @@ export const endpoints = pgTable("endpoints", {
   url: text().notNull(),
   secret: text().notNull(),
   created_at: timestamp().defaultNow(),
+});
+
+export const delivery_attempts = pgTable("delivery_attempts", {
+  id: uuid().defaultRandom().primaryKey(),
+  project_id: uuid()
+    .references(() => projects.id)
+    .notNull(),
+  endpoint_id: uuid()
+    .references(() => endpoints.id)
+    .notNull(),
+  payload: json().notNull(),
+  status_code: integer(),
+  status: status(),
+  attempt_num: integer().default(1),
+  correlation_id: text().unique().notNull(),
+  latency_ms: integer(),
+  attempted_at: timestamp().defaultNow(),
 });
