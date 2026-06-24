8a6eaf122bf4d5511efc27ddfb27df72fd89cc33
Arun Kumar <pheron408@gmail.com>
2026-06-24 16:45:21 +0530

    feat(secret): implement getProjectSecret function to retrieve project secret by projectId

--- packages/shared/src/db/repository/endpoint.repository.ts
+++ packages/shared/src/db/repository/endpoint.repository.ts
@@ -16,3 +16,31 @@ export async function getEndpointAndTargetUrl(
 
   return result[0] ?? null;
 }
+
+/**
+ * this function query database and finds secret for projectId
+ * on success otherwise throw..
+ * @param {string} projectId  pass by client..
+ * @returns {string} secret for the projetId if exist
+ */
+
+export async function getProjectSecret(
+  projectId: string,
+): Promise<{ secret: string }> {
+  let result;
+  try {
+    result = await db
+      .select({ secret: endpoints.secret })
+      .from(endpoints)
+      .where(eq(endpoints.project_id, projectId))
+      .limit(1); //assuming 1 project have 1 secret only....
+  } catch (err) {
+    throw new Error("Can't resolved query", { cause: err });
+  }
+
+  if (result.length === 0) {
+    throw Error("projectId not found");
+  }
+
+  return result[0];
+}
