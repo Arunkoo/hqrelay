d1994b7c77ae8f38b36caca8bc5ffcd5de3d2e3a
Arun Kumar <pheron408@gmail.com>
2026-06-27 20:42:34 +0530

    refactor(rate-limiter): simplify sliding window logic by integrating isRateLimited function and removing manual request tracking

--- apps/receiver/src/middleware/rateLimiter.ts
+++ apps/receiver/src/middleware/rateLimiter.ts
@@ -1,33 +1,7 @@
+import { isRateLimited } from "@hqrelay/shared/src/cache/slidingWindowRateLimit";
 import { Request, Response, NextFunction } from "express";
 
-//constants
-const Window_Ms = 60 * 1000;
-const Max_Requests = 100;
-
-interface RateLimiterEntry {
-  counter: number;
-  windowStart: number;
-}
-
-const reqCount = new Map<string, RateLimiterEntry>(); //placeholder for redis...
-
-function getOrCreateEntry(projectId: string): RateLimiterEntry {
-  if (!reqCount.has(projectId)) {
-    reqCount.set(projectId, { counter: 0, windowStart: Date.now() });
-  }
-  return reqCount.get(projectId) as RateLimiterEntry;
-}
-
-function isWindowExpired(entry: RateLimiterEntry): boolean {
-  return Date.now() - entry.windowStart > Window_Ms;
-}
-
-function resetWindow(entry: RateLimiterEntry): void {
-  entry.counter = 0;
-  entry.windowStart = Date.now();
-}
-
-export function slidingWindowRateLimiter(
+export async function slidingWindowRateLimiter(
   req: Request,
   res: Response,
   next: NextFunction,
@@ -35,20 +9,20 @@ export function slidingWindowRateLimiter(
   const projectId = req.params.projectId as string;
   if (!projectId) return res.status(400).json({ message: "Bad request" });
 
-  const entry = getOrCreateEntry(projectId);
+  try {
+    const isInvalidReq = await isRateLimited(projectId);
+    console.log(`redis resolved value: ${isInvalidReq}`);
 
-  if (isWindowExpired(entry)) resetWindow(entry);
+    if (isInvalidReq) {
+      return res.status(429).json({
+        message: "Too many frequent request",
+      });
+    }
 
-  if (entry.counter >= Max_Requests) {
-    const retryAfterTime = Math.ceil(
-      (entry.windowStart + Window_Ms - Date.now()) / 1000,
-    );
-    return res.status(429).json({
-      message: "Too many requests",
-      retryAfter: `Retry after ${retryAfterTime} seconds.`,
-    });
+    next();
+  } catch (error) {
+    console.error("[Redis]:Client is unable to resolve request");
+
+    return next(); //Important point that my system need available despite of redis down.  fails open method..  hmac there to verify if person is valid or not...
   }
-
-  entry.counter++;
-  next();
 }
