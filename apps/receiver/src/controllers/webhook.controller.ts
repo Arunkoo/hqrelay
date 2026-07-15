ab9d7249ba1e24ce3fe1bfcd8d02f34600ee98c0
Arun Kumar <pheron408@gmail.com>
2026-07-15 16:59:28 +0530

    feat(controller): add struvtured logging and passing log object to downstream

--- apps/receiver/src/controllers/webhook.controller.ts
+++ apps/receiver/src/controllers/webhook.controller.ts
@@ -8,26 +8,30 @@ export async function receiveWebhook(req: Request, res: Response) {
   const correlationId = req.correlationId!;
   const webhookId =
     req.headers["x-webhook-id"] || getFallBack(projectId, payload);
+
   try {
     const queueRes = await queueWebhook(
       projectId,
       payload,
       correlationId,
       webhookId as string,
+      req.logger,
     );
     if (queueRes.queued) {
+      req.logger?.info("webhook received");
       return res.status(202).json({
         message: "webhook received",
         jobId: queueRes.jobId,
       });
     } else {
+      req.logger?.error("Retry again unable to queue webhook");
       return res.status(500).json({
         message: "Retry again unable to queue webhook",
         jobId: queueRes.jobId,
       });
     }
   } catch (error) {
-    console.error("Failed to queue webhook:", error);
+    req.logger?.error({ err: error }, "Internal server error");
     return res.status(500).json({
       message: "Internal server error",
     });
