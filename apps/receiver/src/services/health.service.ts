72bb0d70712f3fa014270f1747b03dd47e552e4c
Arun Kumar <pheron408@gmail.com>
2026-08-03 17:15:58 +0530

    feat(health): implement healthCheck function to assess service liveness

--- apps/receiver/src/services/health.service.ts
+++ apps/receiver/src/services/health.service.ts
@@ -1,1 +1,51 @@
+import { checkRedisLiveness } from "@hqrelay/shared/src/cache/pingRedis";
+import { checkDatabaseLiveness } from "@hqrelay/shared/src/db/repository/health.repository";
+import { checkRabbitMQLiveness } from "@hqrelay/shared/src/queue/checkRabbitMQ";
 
+function toStatus(result: PromiseSettledResult<boolean>): "Up" | "Down" {
+  if (result.status === "rejected") {
+    return "Down";
+  }
+
+  //if status is fullfilled we have 2 cases that either timeout fire or either service throw..
+  if (result.value === false) {
+    return "Down";
+  } else {
+    return "Up";
+  }
+}
+
+export async function healthCheck() {
+  const val = await Promise.allSettled([
+    checkDatabaseLiveness(),
+    checkRabbitMQLiveness(),
+    checkRedisLiveness(),
+  ]);
+
+  const dbVal = toStatus(val[0]);
+  const rabbitmqVal = toStatus(val[1]);
+  const redisVal = toStatus(val[2]);
+
+  const checks = {
+    postgres: dbVal,
+    rabbitMq: rabbitmqVal,
+    redis: redisVal,
+  };
+  //redisval is non critical but dbVal and rabbitmqVal that is critical for this system..
+  if (dbVal === "Down" || rabbitmqVal === "Down") {
+    return {
+      status: "down",
+      checks: checks,
+    };
+  } else if (redisVal == "Down") {
+    return {
+      status: "degraded",
+      checks: checks,
+    };
+  } else {
+    return {
+      status: "ok",
+      checks: checks,
+    };
+  }
+}
